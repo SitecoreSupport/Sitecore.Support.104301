bab1ab1807a7d274cb3def1ba9c1f12fa0dca84b
sc-ido <ido@sitecore.net>
2018-06-11 18:17:17 +0300

    Patch rewriting from 8.0.3 to 8.2.7

---
+++ src/Sitecore.Support.104301/ContentSearch/SitecoreItemCrawler.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ContentSearch;
+using System.Linq;
+
+namespace Sitecore.Support.ContentSearch
+{
+  public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
+  {
+    protected override void UpdateDependents(IProviderUpdateContext context, SitecoreIndexableItem indexable)
+    {
+      foreach (var uniqueId in GetIndexingDependencies(indexable).Where(i => !this.IsExcludedFromIndex(indexable, true)))
+      {
+        if (!this.CircularReferencesIndexingGuard.IsInProcessedList(uniqueId, this, context))
+        {
+          foreach (IProviderCrawler crawler in this.Index.Crawlers)
+            crawler.Update(context, uniqueId, IndexingOptions.Default);
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.104301/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.104301")]
+[assembly: AssemblyProduct("Sitecore.Support.104301")]
+[assembly: ComVisible(false)]
